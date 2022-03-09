e95c4a96644a037ccae925bd297e8726a21b02a1
Joe Charlier <github@aepryus.com>
2022-03-09 21:58:33 +0900

    things made public

--- Sources/OoviumEngine/Aether/Also/Also.swift
+++ Sources/OoviumEngine/Aether/Also/Also.swift
@@ -13,25 +13,6 @@ public class Space {}
 public final class Also: Aexel {
 	@objc public var aetherPath: String = ""
 
-	public var spaceAether: (Space, Aether)? {
-		return nil
-//		guard let (space, aether) = Space.digest(aetherPath: aetherPath) else { return nil }
-//		return (space, aether)
-	}
-
-	public var aetherName: String {
-		return ""
-//		Space.split(aetherPath: aetherPath).1
-	}
-	public var alsoAether: Aether? {
-		return nil
-//		spaceAether?.1
-	}
-
-	public var functionCount: Int {
-		return alsoAether?.functions(not: [aether]).count ?? 0
-	}
-	
 // Aexel ===========================================================================================
 //	override var towers: Set<Tower> {}
 	
