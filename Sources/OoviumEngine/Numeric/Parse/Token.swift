c7b6364a3264988d7ec55b4c73842c7d29e36571
Joe Charlier <github@aepryus.com>
2022-03-09 19:11:25 +0900

    things made public

--- Sources/OoviumEngine/Numeric/Parse/Token.swift
+++ Sources/OoviumEngine/Numeric/Parse/Token.swift
@@ -38,14 +38,14 @@ public enum TokenStatus {
 	case ok, invalid, deleted, blocked
 }
 
-protocol Labelable: Token { var label: String { get set } }
-protocol Paramsable: Token { var params: Int { get set } }
-protocol Levelable: Token { var level: TokenLevel { get } }
-protocol Defable: Token { var def: Def? { get set } }
+public protocol Labelable: Token { var label: String { get set } }
+public protocol Paramsable: Token { var params: Int { get set } }
+public protocol Levelable: Token { var level: TokenLevel { get } }
+public protocol Defable: Token { var def: Def? { get set } }
 
 public class TowerToken: Token, Labelable, Defable {
 	public var label: String
-	var def: Def? = nil
+	public var def: Def? = nil
 
 	fileprivate init(type: TokenType, tag: String, label: String? = nil) {
 		self.label = label ?? tag
@@ -63,7 +63,7 @@ public class SeparatorToken: Token {
 	init(tag: String) { super.init(type: .separator, tag: tag) }
 }
 public class ConstantToken: Token, Defable {
-	var def: Def? = nil
+	public var def: Def? = nil
 
 	init(tag: String) {
 		super.init(type: .constant, tag: tag)
@@ -75,8 +75,8 @@ public class UnaryToken: Token {
 	}
 }
 public class OperatorToken: Token, Labelable, Levelable {
-	var label: String
-	let level: TokenLevel
+	public var label: String
+	public let level: TokenLevel
 
 	init(tag: String, level: TokenLevel, label: String? = nil) {
 		self.label = label ?? tag
@@ -86,7 +86,7 @@ public class OperatorToken: Token, Labelable, Levelable {
 	public override var display: String { return label }
 }
 public class FunctionToken: TowerToken, Paramsable {
-	var params: Int
+	public var params: Int
 	let recipe: String?
 	init(tag: String, label: String? = nil, params: Int = 1, recipe: String? = nil) {
 		self.params = params
